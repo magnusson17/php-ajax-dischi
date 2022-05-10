8f15e3c0d85c49569a4070b0608e8b70ed963282
Ga <gabri.magni.97@gmail.com>
2022-05-10 11:13:05 +0200

    change folder

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,12 @@ var app = new Vue({
     el: '#app',
     data: {
       arrayElementi: [],
-
       selectGenre: '',
     },
 
     
     created(){
-      axios.get( 'http://localhost/php-ajax-dischi/assets/api/integers.php' )
+      axios.get( 'http://localhost/php-ajax-dischi/milestone2/assets/api/integers.php' )
       .then((res) => {
         console.log(res.data);
         this.arrayElementi = res.data;
--- index.php
+++ index.php
@@ -9,55 +9,6 @@ Attraverso l’utilizzo di axios: al caricamento della pagina axios chiederà, a
 Attraverso un’altra chiamata api, filtrare gli album per genere
  -->
 
-
-<!-- 1) butto tutto dentro index -->
-<!-- <?php
-$database = [
-        [
-            'title' => 'New Jersey',
-            'author' => 'Bon Jovi',
-            'year' => 1988,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
-            'genre' => 'Rock'
-        ],
-        [
-            'title' => 'Live at Wembley 86',
-            'author' => 'Queen',
-            'year' => 1992,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
-            'genre' => 'Pop'
-        ],
-        [
-            'title' => 'Ten\'s Summoner\'s Tales',
-            'author' => 'Sting',
-            'year' => 1993,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
-            'genre' => 'Pop'
-        ],
-        [
-            'title' => 'Steve Gadd band',
-            'author' => 'Steve Gadd Band',
-            'year' => 2018,
-            'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
-            'genre' => 'Jazz'
-        ],
-        [
-            'title' => 'Brave new World',
-            'author' => 'Iron Maiden',
-            'year' => 2000,
-            'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg',
-            'genre' => 'Metal'
-        ],
-        [
-            'title' => 'One more car, one more rider',
-            'author' => 'Eric Clapton',
-            'year' => 2002,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
-            'genre' => 'Rock'
-        ]
-    ];
-?> -->
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -70,19 +21,6 @@ $database = [
     <title>Document</title>
 </head>
 <body>
-
-<!-- 1) prendo i dati dentro l'index e li faccio ciclare e stampare -->
-    <!-- <?php
-    foreach($database as $items) {
-        foreach($items as $key => $item) {
-            if ($key == 'poster') {
-                echo "<img src='$item'>" ;
-            } elseif ($key != 'poster') {
-                echo "<p>" . $item . "</p>";
-            }
-        }
-    }
-    ?> -->
 
     <div id="app" class="bg_1E2D3B">
         <div>
